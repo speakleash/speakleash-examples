10e96a90dd1363d999d3825ef06f24790c596990
Szymon Baczyński <simon.baczynski@gmail.com>
2023-07-31 04:03:00 +0200

    Examples refactoring (1/2/3) - based on commits to Issue #12

--- example_1_basic_read/main.py
+++ example_1_basic_read/main.py
@@ -1,14 +1,25 @@
-from speakleash import Speakleash
+"""Speakleash: basic read of metadata and documents"""
+
 import os
 
-base_dir = os.path.join(".")
-replicate_to = os.path.join(base_dir, "datasets")
-sl = Speakleash(replicate_to)
+from speakleash import Speakleash
 
-for ds in sl.datasets:
-    print(ds.name)
-    for doc in ds.ext_data:
-        txt, meta = doc
-        print(txt)
+
+if __name__ == '__main__':
+
+    base_dir = os.path.join(".")
+    replicate_to = os.path.join(base_dir, "datasets")
+    print("Replicate to:", replicate_to)
+    sl = Speakleash(replicate_to)
+
+    # Iterate over Speakleash datasets
+    for ds in sl.datasets:
+        print(ds.name)
+
+        # Import dataset and iterate through documents
+        for doc in ds.ext_data:
+            txt, meta = doc
+            print(f"Metadata: \n{meta}\n")
+            print(f"Docs: \n{txt[:500]}[...]\n")
+            break
         break
-    break
--- example_2_inventory_check/main.py
+++ example_2_inventory_check/main.py
@@ -1,52 +1,69 @@
-"""Speakleash datasets inventory"""
+"""Speakleash: datasets inventory check - table"""
+
 import os
 
 from speakleash import Speakleash
 from termcolor import colored
+from prettytable import PrettyTable
 
-base_dir = os.path.join(".")
-replicate_to = os.path.join(base_dir, "datasets")
 
-sl = Speakleash(replicate_to)
+if __name__ == '__main__':
 
-print("name;size;extraction type;copyrigth label;license;license url")
-  
-for d in sl.datasets:
-    
-    manifest = d.manifest
-    
-    #Identification of extraction type
-    extraction_type = manifest.get("type", "")
-    formatted_extraction_type = colored("n/a", 'red')
+    base_dir = os.path.join(".")
+    replicate_to = os.path.join(base_dir, "datasets")
+    print("\nCollecting information from SpeakLeash database...\n")
+    sl = Speakleash(replicate_to)
 
-    if extraction_type == "speakleash_crawls":
-        formatted_extraction_type = colored("scraped", 'yellow')
+    # Create Pretty Table and set columns names
+    inventory_table = PrettyTable()
+    inventory_table.field_names = ["dataset name", "size [MB]", "extraction type",
+                                    "copyrigth label", "license", "license_url"]
 
-    if extraction_type == "external_dataset":
-        formatted_extraction_type = colored("external dataset", 'blue')
+    for d in sl.datasets:
+        # Extract manifest for each dataset
+        manifest = d.manifest
 
-    #Size of dataset
-    size_mb = round(d.characters/1024/1024)
+        # Identification of extraction type
+        extraction_type = manifest.get("type", "")
+        formatted_extraction_type = colored("n/a", "red")
 
-    #Identification of the copyright description method
-    copyrigth_label = colored("OLD", 'red')
-    disclaimer  = manifest.get("disclaimer", "")
+        if extraction_type == "speakleash_crawls":
+            formatted_extraction_type = colored("scraped", "yellow")
 
-    if extraction_type == "speakleash_crawls":
-        if len(disclaimer) > 0:
-            copyrigth_label = colored("NEWEST", 'blue')
+        if extraction_type == "external_dataset":
+            formatted_extraction_type = colored("external dataset", "blue")
+
+        # Size of dataset
+        size_mb = round(d.characters / 1024 / 1024)
+
+        # Identification of the copyright description method
+        copyrigth_label = colored("OLD", "red")
+        disclaimer = manifest.get("disclaimer", "")
+
+        if extraction_type == "speakleash_crawls":
+            if len(disclaimer) > 0:
+                copyrigth_label = colored("NEWEST", "blue")
+            else:
+                copyrigth_label = colored("OLD", "red")
         else:
-            copyrigth_label = colored("OLD", 'red')
-    else:
-        copyrigth_label = colored("standard", 'green')
+            copyrigth_label = colored("standard", "green")
 
-    sources = manifest.get("sources", [])
+        # Identification of the license and license url
+        sources = manifest.get("sources", [])
+        if len(sources) == 1:
+            licence = (sources[0].get("license", colored("n/a", "red"))
+                        .replace("/n", "").replace(";", ",").strip())
+            license_url = sources[0].get("license_url", "n/a")
+        else:
+            licence = "multiple"
+            license_url = "n/a"
 
-    if len(sources) == 1:
-        licence = sources[0].get("license", colored("n/a", 'red')).replace("/n", "").replace(";",",").strip()
-        license_url = sources[0].get("license_url", "n/a")
-    else:
-        licence = "multiple"
-        license_url = "n/a"
+        # print("{0};{1} MB;{2};{3};{4};{5}".format(colored(d.name,'green'), size_mb,
+        #           formatted_extraction_type, copyrigth_label, licence, license_url))
+        inventory_table.add_row([colored(d.name, "green"), f"{size_mb:,}".replace(",", " "),
+                                 formatted_extraction_type, copyrigth_label,
+                                 licence[:30], license_url[:30],])
 
-    print("{0};{1} MB;{2};{3};{4};{5}".format(colored(d.name,'green'), size_mb, formatted_extraction_type, copyrigth_label, licence, license_url))
+
+    inventory_table.align["size [MB]"] = "r"
+    print(inventory_table)

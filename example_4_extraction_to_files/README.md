72a122d9898f1804e9c995a3db748c6eedfa5fa8
Marek Garbowski <marek.garbowski@proton.me>
2023-09-03 21:52:19 +0200

    Add extraction of high-quality docs to text files (#17)
    * Add extraction of high-quality docs to text files
    
    This commit introduces a new function save_high_quality_docs() in the extraction_to_txt_file.py. It saves high-quality documents from the Speakleash dataset to a specified folder, creating the necessary directories if they don't exist. The .gitignore file has been updated to exclude the output directory from the repository. This function helps in segregating high quality documents for further analysis.
    
    * Update extraction_to_txt_file script for versatility
    
    The extraction_to_txt_file script has been revised for added versatility. A new module has been added providing functions to create necessary directories and save documents from the 'speakleash' dataset to a specific folder. The 'save_high_quality_docs' method was updated to 'save_quality_docs' to allow for selection of document quality. New usage information was also added for guidance on the script modification, importing and using the module's functions, running the script directly, and the output from the script execution. A 'get_data' function was also added to manage the creation of necessary directories. This update provides improved script functionality and versatile document retrieval.
    
    * Update docstring position in extraction_to_txt_file methods
    
    The commit relocates the docstrings to be directly under the method definition, as per Python's standard practice, in the 'extraction_to_txt_file' script. These provide information about the functions 'get_data' and 'save_quality_docs'. This revision ensures that the method's purpose is clarified to the developers in the correct position as a part of Python's best practices. This doesn't affect functionality but improves script readability.
    Removed .gitignore file from repo
    
    * Update docstring position in extraction_to_txt_file methods
    
    The commit relocates the docstrings to be directly under the method definition, as per Python's standard practice, in the 'extraction_to_txt_file' script. These provide information about the functions 'get_data' and 'save_quality_docs'. This revision ensures that the method's purpose is clarified to the developers in the correct position as a part of Python's best practices. This doesn't affect functionality but improves script readability.
    Removed .gitignore file from repo
    
    * Commit message:
        Relocate imports in extraction_to_txt_file.py
    
    This commit moves import statements (os and speakleash) to after module-level docstring in extraction_to_txt_file.py.
    
    * Add README.md for "extraction_to_files" module, refactor related script, and move files to a separate folder.
    
    * Modified the readme.md and main files as suggested.
    
    * Update README.md
    
    ---------
    
    Co-authored-by: marekgarbowski <marekgarbowski@proton.me>
    Co-authored-by: IgorTest19 <58894110+IgorTest19@users.noreply.github.com>

---
+++ example_4_extraction_to_files/README.md
@@ -0,0 +1,3 @@
+# Example No. 4
+
+Example shows how to extract high quality documents from selected dataset and provides functionalities to create necessary directories and save documents of specified quality from the speakleash dataset to a specific folder location.
---
+++ example_4_extraction_to_files/main.py
@@ -0,0 +1,77 @@
+"""
+This module provides functionalities to create necessary directories and save documents 
+of specified quality from the speakleash dataset to a specified folder.
+
+Use `get_data(txt_folder_name)` function for creating directories and 
+`save_quality_docs(txt_folder_name, quality)` for saving documents of a given quality.
+
+Example usage:
+
+1. In the `save_quality_docs` function, specify the name of the dataset to be downloaded 
+   by setting the `quality` parameter.
+2. Set the `limit` variable in the `save_quality_docs` function to determine how many documents you want to retrieve.
+3. Import the module and use the provided functions in your own Python script.
+4. Run this script directly from the command line.
+5. If not present, a folder named "datasets" will be created in the script's directory to store the downloaded dataset.
+6. If not present, another folder named "datasets_high_quality_txt" will be created to store high-quality text files.
+
+This usage information covers the main aspects of the script, including the modification of key parameters, 
+importing and using the module's functions, directly running the script, 
+and the output resulting from running the script.
+"""
+
+
+import os
+from speakleash import Speakleash
+
+
+def get_data(txt_folder_name):
+    """
+    This method creates the necessary directories if they don't already exist.
+
+    :param txt_folder_name: The name of the folder where the high-quality documents will be saved.
+    :return: None
+    """
+    base_dir = os.path.join(".")
+    speakleash_dir = os.path.join(base_dir, "datasets")
+    replicate_to_txt = os.path.join(base_dir, txt_folder_name)
+
+    if not os.path.exists(speakleash_dir):
+        os.makedirs(speakleash_dir)
+    if not os.path.exists(replicate_to_txt):
+        os.makedirs(replicate_to_txt)
+
+
+def save_quality_docs(txt_folder_name, quality='HIGH'):
+    """
+    This method saves documents of the specified quality from the Speakleash dataset to a specified folder.
+
+    :param txt_folder_name: The name of the folder where the documents will be saved.
+    :param quality: The judge quality of the documents.
+    :return: None
+    """
+    base_dir = os.path.join(".")
+    speakleash_dir = os.path.join(base_dir, "datasets")
+    replicate_to_txt = os.path.join(base_dir, txt_folder_name)
+
+    sl = Speakleash(speakleash_dir)
+    name = "plwiki"
+    limit = 10
+    counter = 0
+    ds = sl.get(name).ext_data
+
+    for doc in ds:
+        txt, meta = doc
+        if meta.get("quality", "") == quality:
+            print(f"{quality}-quality document")
+            with open(os.path.join(replicate_to_txt, f'{quality}_quality_doc_{counter}.txt'), 'w') as out_file:
+                out_file.write(txt)
+            counter += 1
+            if counter > limit:
+                break
+
+
+if __name__ == "__main__":
+    txt_folder_name = "datasets_high_quality_txt"
+    get_data(txt_folder_name)
+    save_quality_docs(txt_folder_name, "HIGH")

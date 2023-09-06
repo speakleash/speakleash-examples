7bf48636710b3eca8c307e281a968544b43e90bc
Szymon Baczyński <simon.baczynski@gmail.com>
2023-09-06 12:37:15 +0200

    Add comments + dataset change + unified WordCloud visualization settings

--- example_4_word_cloud/main_1_spacy.py
+++ example_4_word_cloud/main_1_spacy.py
@@ -9,37 +9,44 @@ import spacy
 from wordcloud import WordCloud
 
 
-
 if __name__ == '__main__':
 
-    PROJECT = "saos"
+    # Select dataset ("wolne_lektury_corpus" is smaller one)
+    PROJECT = "wolne_lektury_corpus"
 
+    # Initiating directory
     base_dir = os.path.join(os.path.dirname(__file__))
     replicate_to = os.path.join(base_dir, "datasets")
+    print(f"Replicate datasets to: {replicate_to}")
 
+    # Initiating Speakleash
     sl = Speakleash(replicate_to)
+
+    # Get only text from dataset
     ds = sl.get(PROJECT).data
 
-    wc = WordCloud()
-
+    # Prepare spacy pipe & set maximum text length (considering RAM allocation)
     nlp = spacy.load('pl_core_news_md', disable=["ner", "parser", "tagger", "textcat"])
     nlp.max_length = 200000
 
-    counts_all = Counter()
+    # Prepare texts & process with spacy
     limit = 100
-
-    # TODO!: Read only document len() < nlp.max_length
-
     texts = [txt[:nlp.max_length] for _, txt in zip(range(limit), ds)]
     docs = list(nlp.pipe(texts))
 
-    for doc in docs:
+    # Process text and update frequency distribution
+    wc = WordCloud(max_font_size=100, max_words=100, background_color="white", contour_width=1)
+    counts_all = Counter()
+
+    for _, doc in enumerate(docs):
         processed_txt = ' '.join([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
         counts_line = wc.process_text(processed_txt)
         counts_all.update(counts_line)
 
+    # Create wordcloud
     wc.generate_from_frequencies(counts_all)
     
+    # Display wordcloud
     plt.figure()
     plt.imshow(wc, interpolation="bilinear")
     plt.axis("off")
--- example_4_word_cloud/main_2_nltk.py
+++ example_4_word_cloud/main_2_nltk.py
@@ -11,7 +11,6 @@ from wordcloud import WordCloud
 # Stopwords list (from Wikipedia)
 stopwords_dict = ["a", "aby", "ach", "acz", "aczkolwiek", "aj", "albo", "ale", "ależ", "ani", "aż", "bardziej", "bardzo", "bo", "bowiem", "by", "byli", "bynajmniej", "być", "był", "była", "było", "były", "będzie", "będą", "cali", "cała", "cały", "ci", "cię", "ciebie", "co", "cokolwiek", "coś", "czasami", "czasem", "czemu", "czy", "czyli", "daleko", "dla", "dlaczego", "dlatego", "do", "dobrze", "dokąd", "dość", "dużo", "dwa", "dwaj", "dwie", "dwoje", "dziś", "dzisiaj", "gdy", "gdyby", "gdyż", "gdzie", "gdziekolwiek", "gdzieś", "i", "ich", "ile", "im", "inna", "inne", "inny", "innych", "iż", "ja", "ją", "jak", "jaka", "jakaś", "jakby", "jaki", "jakichś", "jakie", "jakiś", "jakiż", "jakkolwiek", "jako", "jakoś", "je", "jeden", "jedna", "jedno", "jednak", "jednakże", "jego", "jej", "jemu", "jest", "jestem", "jeszcze", "jeśli", "jeżeli", "już", "ją", "każdy", "kiedy", "kilka", "kimś", "kto", "ktokolwiek", "ktoś", "która", "które", "którego", "której", "który", "których", "którym", "którzy", "ku", "lat", "lecz", "lub", "ma", "mają", "mało", "mam", "mi", "mimo", "między", "mną", "mnie", "mogą", "moi", "moim", "moja", "moje", "może", "możliwe", "można", "mój", "mu", "musi", "my", "na", "nad", "nam", "nami", "nas", "nasi", "nasz", "nasza", "nasze", "naszego", "naszych", "natomiast", "natychmiast", "nawet", "nią", "nic", "nich", "nie", "niech", "niego", "niej", "niemu", "nigdy", "nim", "nimi", "niż", "no", "o", "obok", "od", "około", "on", "ona", "one", "oni", "ono", "oraz", "oto", "owszem", "pan", "pana", "pani", "po", "pod", "podczas", "pomimo", "ponad", "ponieważ", "powinien", "powinna", "powinni", "powinno", "poza", "prawie", "przecież", "przed", "przede", "przedtem", "przez", "przy", "roku", "również", "sama", "są", "się", "skąd", "sobie", "sobą", "sposób", "swoje", "ta", "tak", "taka", "taki", "takie", "także", "tam", "te", "tego", "tej", "temu", "ten", "teraz", "też", "to", "tobą", "tobie", "toteż", "trzeba", "tu", "tutaj", "twoi", "twoim", "twoja", "twoje", "twym", "twój", "ty", "tych", "tylko", "tym", "u", "w", "wam", "wami", "was", "wasz", "wasza", "wasze", "we", "według", "wiele", "wielu", "więc", "więcej", "wszyscy", "wszystkich", "wszystkie", "wszystkim", "wszystko", "wtedy", "wy", "właśnie", "z", "za", "zapewne", "zawsze", "ze", "zł", "znowu", "znów", "został", "żaden", "żadna", "żadne", "żadnych", "że", "żeby"]
 
-
 # Function to Tokenize the text
 def tokenize_text(text: str) -> list[str]:
     '''
@@ -32,20 +31,20 @@ def tokenize_text(text: str) -> list[str]:
     return [word for word in words if (word.isalpha() and word not in stopwords_dict)]
 
 
-
 if __name__ == '__main__':
 
-    PROJECT = "saos"
+    # Select dataset ("wolne_lektury_corpus" is smaller one)
+    PROJECT = "wolne_lektury_corpus"
 
     # Initiating directory
-    base_dir = os.path.join(".")
+    base_dir = os.path.join(os.path.dirname(__file__))
     replicate_to = os.path.join(base_dir, "datasets")
     print(f"Replicate datasets to: {replicate_to}")
 
     # Initiating Speakleash
     sl = Speakleash(replicate_to)
 
-    # Get dataset
+    # Get only text from dataset
     data = sl.get(PROJECT).data
 
     limit = 100
@@ -67,17 +66,7 @@ if __name__ == '__main__':
                     background_color="white",
                     contour_width=1).generate_from_frequencies(word_freq)
 
-    # # Print 'word_freq' - look into it
-    # sorted_items = sorted(word_freq.items(), key=lambda item: item[1], reverse=True)
-    # # Define the start and end indices for the slice
-    # start_index = 1
-    # end_index = 20
-    # # Create a dictionary slice using dictionary comprehension
-    # dict_slice = {key: value for key, value in sorted_items[start_index:end_index]}
-    # print(dict_slice)
-
-
-    # Display word cloud
+    # Display wordcloud
     plt.figure(figsize=(10, 5))
     plt.imshow(wc, interpolation='bilinear')
     plt.axis("off")

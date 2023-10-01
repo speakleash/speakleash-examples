71b9f7590ad28ea2051b4fcf4763e15fdd0b6fbe
SeKon <87979883+SensXYZ@users.noreply.github.com>
2023-10-01 22:24:45 +0200

    First slow version (#19)
    * First slow version
    
    * Create new files - 2 versions (mod. SpaCy + NLTK)
    
    * Changed default dataset (problems with 'form_symfonik' )
    
    * Text length limit (problem with memory allocation)
    
    * Add comments + dataset change + unified WordCloud visualization settings
    
    * Rename folder + add README.md + add info about installation (spaCy)
    
    * Changed to include suggestions & deleting an unnecessary folder
    
    ---------
    
    Co-authored-by: Sebastian Kondracki <sebastian.kondracki@deviniti.com>
    Co-authored-by: Szymon Baczyński <simon.baczynski@gmail.com>

---
+++ example_5_word_cloud/README.md
@@ -0,0 +1,5 @@
+# Example No. 4
+
+There are 2 examples of Word Cloud creation in this folder:
+- *main_1_spacy.py* - an example using the spaCy library, using the lemmatizer. To avoid the high RAM usage problem, the processed documents are limited to a set number of characters.
+- *main_2_nltk.py* - example using the NLTK library. The script is faster than the version using spaCy but takes into account most words (excluding Polish stopwords).
---
+++ example_5_word_cloud/main_1_spacy.py
@@ -0,0 +1,58 @@
+"""Speakleash: basic wordcloud (using spacy)"""
+
+import os
+from collections import Counter
+
+import matplotlib.pyplot as plt
+import spacy
+from speakleash import Speakleash
+from wordcloud import WordCloud
+
+# For the script to work you need to download and install the spacy model: 
+# - included in "requirements.txt" -> pip install -r requirements.txt
+# - or available from the link:
+# https://github.com/explosion/spacy-models/releases/download/pl_core_news_md-3.5.0/pl_core_news_md-3.5.0-py3-none-any.whl
+
+
+if __name__ == '__main__':
+
+    # Select dataset ("wolne_lektury_corpus" is smaller one)
+    PROJECT = "wolne_lektury_corpus"
+
+    # Initiating directory
+    base_dir = os.path.join(os.path.dirname(__file__))
+    replicate_to = os.path.join(base_dir, "datasets")
+    print(f"Replicate datasets to: {replicate_to}")
+
+    # Initiating Speakleash
+    sl = Speakleash(replicate_to)
+
+    # Get only text from dataset
+    ds = sl.get(PROJECT).data
+
+    # Prepare spacy pipe & set maximum text length (considering RAM allocation)
+    nlp = spacy.load('pl_core_news_md', disable=["ner", "parser", "tagger", "textcat"])
+    nlp.max_length = 200000
+
+    # Prepare texts & process with spacy
+    limit = 100
+    texts = [txt[:nlp.max_length] for _, txt in zip(range(limit), ds)]
+    docs = list(nlp.pipe(texts))
+
+    # Process text and update frequency distribution
+    wc = WordCloud(max_font_size=100, max_words=100, background_color="white", contour_width=1)
+    counts_all = Counter()
+
+    for _, doc in enumerate(docs):
+        processed_txt = ' '.join([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
+        counts_line = wc.process_text(processed_txt)
+        counts_all.update(counts_line)
+
+    # Create wordcloud
+    wc.generate_from_frequencies(counts_all)
+    
+    # Display wordcloud
+    plt.figure()
+    plt.imshow(wc, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
